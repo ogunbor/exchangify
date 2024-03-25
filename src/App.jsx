44304af7c21901a9c941839886ba11aac8a14fb8
mompei <ogunborjerry@gmail.com>
2024-03-25 23:47:03 +0100

    Timeline component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-// App.js
-
-import React, { useEffect } from 'react';
+import React from 'react';
 import { MdNightsStay } from "react-icons/md";
 import Navbar from './Navbar';
 import TradeNowButton from './WhatsAppButton';
 import Welcome from './Welcome';
 import Copyright from './Copyright';
 import './App.css'; 
+import ColorsTimeLine from './ColorsTimeLine';
+
+
 
 const App = () => {
-  useEffect(() => {
-    const setViewportHeight = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    };
-
-    setViewportHeight(); // Set initial height
-
-    window.addEventListener('resize', setViewportHeight);
-
-    return () => {
-      window.removeEventListener('resize', setViewportHeight);
-    };
-  }, []);
-
   return (
-    <div className="container">
+    <div className="hero-section">
       <Navbar />
       <Welcome />
-      <TradeNowButton />
-      <Copyright />
+      <ColorsTimeLine/>
+      <TradeNowButton/>
+      <Copyright/>
     </div>
   );
 };
---
+++ src/ColorsTimeLine.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+
+export default function ColorsTimeLine() {
+  return (
+    <Timeline position="alternate">
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot color="secondary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Click below</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot color="success" />
+        </TimelineSeparator>
+        <TimelineContent>We serve you</TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
+}
