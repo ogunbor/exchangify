9afa348ce4b76ccc176d3fe6127718f23171d160
mompei <ogunborjerry@gmail.com>
2024-03-25 23:05:06 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+// App.js
+
+import React, { useEffect } from 'react';
 import { MdNightsStay } from "react-icons/md";
 import Navbar from './Navbar';
 import TradeNowButton from './WhatsAppButton';
@@ -7,12 +9,27 @@ import Copyright from './Copyright';
 import './App.css'; 
 
 const App = () => {
+  useEffect(() => {
+    const setViewportHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    setViewportHeight(); // Set initial height
+
+    window.addEventListener('resize', setViewportHeight);
+
+    return () => {
+      window.removeEventListener('resize', setViewportHeight);
+    };
+  }, []);
+
   return (
-    <div>
+    <div className="container">
       <Navbar />
       <Welcome />
-      <TradeNowButton/>
-      <Copyright/>
+      <TradeNowButton />
+      <Copyright />
     </div>
   );
 };
